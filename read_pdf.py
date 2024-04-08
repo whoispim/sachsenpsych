7355bcd82311212a258791b7580fbd72c45eb621
whoispim <pimbot@bk.ru>
2024-04-08 16:38:07 +0200

    arg files, added leistungen

--- read_pdf.py
+++ read_pdf.py
@@ -1,12 +1,16 @@
+import sys
 import pypdf
-import re
 from datetime import time
 
-reader = pypdf.PdfReader('Detailansicht.pdf')
+# if len(sys.argv) <= 1:
+#     print('Please enter file names of Detailansicht.pdf-files.')
+#     exit()
 
-# print(len(reader.pages))
-# name, adresse, nummer, erreichbarkeit, fachgebiet, leistungen
-
+# psy_pages = []
+# for i in range(1, len(sys.argv)):
+#     reader = pypdf.PdfReader(sys.argv[i])
+#     psy_pages.extend([p.extract_text() for p in reader.pages])
+reader = pypdf.PdfReader('Detailansicht_04103.pdf')
 psy_pages = [p.extract_text() for p in reader.pages]
 # print(psy_pages)
 
@@ -28,11 +32,17 @@ for p in psy_pages:
                 time(*[int(x) for x in a.split(':')]), 
                 time(*[int(x) for x in b.split(':')])
                 ])
+    p_leistungen = ''
+    for line in p.split('Genehmigungspflichtige\nLeistungen: ')[1].split('\n'):
+        if 'Die Leistung wird' in line or 'Fremdsprache: ' in line:
+            break
+        p_leistungen += line.replace('Psychotherapie: ', '') + ', '
+    p_leistungen = p_leistungen[:-2]
+    
     print(f'Name:       {p_name}')
     print(f'Adresse:    {p_praxis}')
     print(f'Fachgebiet: {p_fachgebiet}')
     print(f'Telefon:    {p_telefon}')
     print(f'Erreichbar: {p_erreichbarkeit}')
+    print(f'Leistungen: {p_leistungen}')
     print()
-        
-        
